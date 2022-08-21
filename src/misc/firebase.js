1f48d2c4293ceda255412e8a45fed7ca5be6d077
Akash <ak668463@gmail.com>
2022-08-22 05:19:46 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const Config = {
+    apiKey: "AIzaSyC0tFs-BpWAUtF6J8N2IU13drraowbHrwE",
+    authDomain: "chat-web-app-3b6a5.firebaseapp.com",
+    projectId: "chat-web-app-3b6a5",
+    storageBucket: "chat-web-app-3b6a5.appspot.com",
+    messagingSenderId: "968048149369",
+    appId: "1:968048149369:web:ea4ef7cd4208a4e55f25a8"
+  };
+
+const app =   firebase.initializeApp(Config);
