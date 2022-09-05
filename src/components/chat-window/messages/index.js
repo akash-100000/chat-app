d4ddbf44458cbd10ed1fc3cdd0581b4f93b0c838
Akash <ak668463@gmail.com>
2022-09-05 15:10:02 +0530

    displayed audio messages and fixed deleted file to also get deleted from database

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { auth, database } from '../../../misc/firebase';
+import { auth, database, storage } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -82,7 +82,7 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   },[]);
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId,file) => {
       // eslint-disable-next-line no-alert
       if (!window.confirm('Delete this message?')) {
         return;
@@ -110,7 +110,16 @@ const Messages = () => {
 
         Alert.info('Message has been deleted');
       } catch (err) {
-        Alert.error(err.message);
+        // eslint-disable-next-line consistent-return
+        return Alert.error(err.message);
+      }
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message);
+        }
       }
     },
     [chatId, messages]
